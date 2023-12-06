52ffe6e05b9a23c3166807b0ba3ab9ef41c83b1a
TotallyNotNero <neromisis2015@gmail.com>
2023-12-06 01:12:14 -0600

    comments and other good-looking stuff

--- src/main/java/frc/robot/utils/Constants.java
+++ src/main/java/frc/robot/utils/Constants.java
@@ -1,25 +1,27 @@
 package frc.robot.utils;
 
 public final class Constants {
-    //QOL
-    public static final double TAU = 2 * Math.PI;
-    public static final double RA = Math.PI / 4;//RIght angle
 
-    //DRIVE TRAIN
+    // QOL
+    public static final double TAU = 2 * Math.PI;
+    public static final double RA = Math.PI / 4; // Right angle
+
+    // SWERVE DRIVE
     public static final double DRIVERATIO = 6.12;
-    public static final double STEERRATIO = 150/7;
-    public static final double WHEELDIAMETER = 4.0;//inches
+    public static final double STEERRATIO = 150 / 7;
+    public static final double WHEELDIAMETER = 4.0; // Inches
     public static final double FRAMEWIDTH = 25.0;
     public static final double FRAMELENGTH = 25.0;
     public static final double WHEELBASEWIDTH = FRAMEWIDTH - 2 * 2.65;
     public static final double WHEELBASELENGTH = FRAMELENGTH - 2 * 2.65;
 
-    public static final double FLCANCODEROFFSET = 0.0;
-    public static final double FRCANCODEROFFSET = 0.0;
-    public static final double BLCANCODEROFFSET = 0.0;
-    public static final double BRCANCODEROFFSET = 0.0;
+    // CANCODER OFFSETS
+    public static final double FLOFFSET = 0.0;
+    public static final double FROFFSET = 0.0;
+    public static final double BLOFFSET = 0.0;
+    public static final double BROFFSET = 0.0;
 
-    //CAN IDs
+    // CAN IDs
     public static final int FLDRIVEID = 0;
     public static final int FRDRIVEID = 0;
     public static final int BLDRIVEID = 0;
